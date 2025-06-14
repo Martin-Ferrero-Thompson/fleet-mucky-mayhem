a0414c84aa81b42e656db7b29583c1f88a4b82fc
Martin Ferrero-Thompson <martin.ferrero.thompson@gmail.com>
2025-06-14 17:10:13 +0200

    v1.4.8 Published

--- src/js/utils.js
+++ src/js/utils.js
@@ -28,7 +28,7 @@ const updateCountdownDisplay = (days, hours, minutes, seconds) => {
  * Stops the countdown when the target date is reached.
  */
 const calculateAndDisplayCountdown = () => {
-    const futureDate = new Date("2025-06-14T09:00:00");
+    const futureDate = new Date("2025-07-05T09:00:00");
     const currentDate = new Date();
     let timeLeft = futureDate.getTime() - currentDate.getTime(); // Use getTime() for milliseconds
 
